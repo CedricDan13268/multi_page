2b65a7db478f707131eef84bfedc2c760a38e5d3
CedricDan13268 <cablayan57cedricdan@gmail.com>
2024-07-23 14:41:05 +0800

    add code

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/blog-home', function () {
+    return view('blog-home');
+});
+
+Route::get('/blog-post', function () {
+    return view('blog-post');
+});
